3b5b10c6803c891bb2fe04e9ac762a179c66c0ef
monkeypineapple <davidrosenholz@gmail.com>
2019-12-12 17:34:06 -0800

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,59 @@
+class Song
+    attr_accessor :name, :artist, :genre, :genre_count
+    @@count = 0
+    @@genres = []
+    @@artists = []
+    @@genre_count = {}
+    @@artist_count = {}
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@genres << genre
+        @@artists << artist
+    end
+
+    # def name
+    #     @name
+    # end
+
+    def self.count
+        @@count
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genre_count
+        @@genres.each do |genre|
+            if @@genre_count.include?(genre)
+             @@genre_count[genre] += 1
+            else
+                @@genre_count[genre] = 1
+            end
+        end
+        @@genre_count
+    end
+
+    def self.artist_count
+        @@artists.each do |artist|
+            if @@artist_count.include?(artist)
+              @@artist_count[artist] += 1
+            else
+              @@artist_count[artist] = 1
+            end
+        end
+        @@artist_count
+    end
+
+
+
+
+
+end
